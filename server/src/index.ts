d245fa818dacede2e3cafa677ec7b4e32d3603f8
kashish <kgupta7827@gmail.com>
2026-05-25 15:32:01 +0530

    feat: initialize server with WebSocket support and TypeScript configurat

---
+++ server/src/index.ts
@@ -0,0 +1,21 @@
+import { WebSocketServer } from 'ws';
+
+const server = new WebSocketServer({
+    port:8081
+})
+
+server.on('connection',(socket)=>{
+    console.log("Client connected")
+
+    socket.on('message',(message)=>{
+        console.log(`Received ${message}`)
+        socket.send(`Server  : ${message}`)
+    });
+
+    socket.on('close',()=>{
+        console.log('Clinet disconnected')
+    })
+})
+
+
+console.log('WebSocket server is running on ws://localhost:8081');
